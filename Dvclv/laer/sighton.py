ea5f823323ab464afbf0ea3690cdc4fdabee4c38
MageniC <168565191+MageniC0@users.noreply.github.com>
2024-07-08 03:25:06 +0800

    Delete Dvclv directory
    Signed-off-by: MageniC <168565191+MageniC0@users.noreply.github.com>

--- Dvclv/laer/sighton.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-filename = input("file name:")
-original_image = Image.open('a2.png')
-enlarged_image = original_image.resize((980, 980),Image.NEAREST)
-enlarged_image.save('filename')
